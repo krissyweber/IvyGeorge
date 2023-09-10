b925533ca87ee0b89f1c410a00270a26b38f1d05
krissyweber18 <krissyweber18@gmail.com>
2023-09-10 17:17:41 -0400

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,66 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+@auth
+
+<p>You are logged in</p>
+<form action="/logout" method="POST">
+    @csrf
+    <button>LOG OUT</button>
+</form>
+<div style="border: 3px solid black;">
+    <h2>Create a New Post</h2>
+    <form action="/create-post" method="POST">
+        @csrf
+        <input name="title" type="text" placeholder="title">
+        <textarea name="body" type="text" placeholder="body content"></textarea>
+        <button>Post</button>
+</form>
+</div>
+<div style="border: 3px solid black;">
+<h2>All Posts</h2>
+@foreach ($posts as $post)
+    <div style="background-color: gray; padding: 10px; margin:10px;">
+        <h3>{{$post['title']}} by {{$post->user->name}}</h3>
+        {{$post['body']}}
+        <p><a href="/edit-post/{{$post->id}}">Edit</a></p>
+        <form action="/delete-post/{{$post->id}}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button>Delete</button>
+    </div>
+@endforeach
+
+
+@else
+<div style="border: 3px solid black;">
+    <h2>Register</h2>
+    <form action="/register" method="POST">
+        @csrf
+        <input name="name" type="text" placeholder="name">
+        <input name="email" type="text" placeholder="email">
+        <input name="password" type="password" placeholder="password">
+        <button>Register</button>
+</form>
+</div>
+<div style="border: 3px solid black;">
+    <h2>Login</h2>
+    <form action="/login" method="POST">
+        @csrf
+        <input name="loginname" type="text" placeholder="name">
+        <input name="loginpassword" type="password" placeholder="password">
+        <button>Log in</button>
+</form>
+</div>
+    
+@endauth
+
+<body>
+
+
+</body>
+</html>
